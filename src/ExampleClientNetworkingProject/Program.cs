d52a969ba70a31962761d5ce30a4a3428913aeb1
Adam Carter <mail@adam-carter.com>
2020-09-10 10:48:01 -0700

    added starter files for class to use.

---
+++ src/ExampleClientNetworkingProject/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExampleClientNetworkingProject
+{
+    class Program
+    {
+        static void SynchronousConnection(string address, int port)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(address, port);
+                BufferedStream stream = new BufferedStream(client.GetStream());
+                BinaryReader reader = new BinaryReader(stream);
+                BinaryWriter writer = new BinaryWriter(stream);
+
+                //TODO: do work!
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: {0}", ex.Message);
+                throw ex;
+            }
+        }
+
+        public static void ContinuousConnection(string address, int port)
+        {
+            TcpClient client;
+            BufferedStream stream;
+            BinaryReader reader;
+            BinaryWriter writer;
+            try
+            {
+                client = new TcpClient(address, port);
+                stream = new BufferedStream(client.GetStream());
+                reader = new BinaryReader(stream);
+                writer = new BinaryWriter(stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: {0}", ex.Message);
+                throw ex;
+            }
+            while(true)
+            {
+                //TODO: do work!
+            }
+        }
+
+        static async Task TaskedConnection(string address, int port)
+        {
+            await Task.Run(() => { SynchronousConnection(address, port); });
+        }
+
+        public static void ThreadedConnection(string address, int port)
+        {
+            ThreadStart ts = () => { ContinuousConnection(address, port); } ;
+            Thread thread = new Thread(ts);
+            thread.Start();
+
+            //if you want to block until the thread is done, call join.  Otherwise, you can
+            //just return
+            //thread.Join();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
